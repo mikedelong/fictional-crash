4a32331a4e1a1fd3b51128d1adc20f74e9622482
Mike DeLong <mikedelong@outlook.com>
2020-08-11 08:31:45 -0400

    feature: separate out the data with no flight number; we want to report these differently

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from time import time
 from pandas import read_csv
 import datetime
 
+from math import isnan
+
 if __name__ == '__main__':
     time_start = time()
     logger = getLogger(__name__, )
@@ -13,11 +15,16 @@ if __name__ == '__main__':
 
     url = 'https://raw.githubusercontent.com/arif-zaman/airplane-crash/master/Airplane_Crashes_Since_1908.csv'
     df = read_csv(filepath_or_buffer=url, parse_dates=['Date'])
-    df['day'] = df.apply(lambda x: '{}-{}'.format(x['Date'].date().month, x['Date'].date().day, ), axis=1,)
+    df['day'] = df.apply(lambda x: '{}-{}'.format(x['Date'].date().month, x['Date'].date().day, ), axis=1, )
 
     today = '{}-{}'.format(datetime.date.today().month, datetime.date.today().day, )
     logger.info(today)
     select_df = df[df.day == today]
     logger.info('crashes on this day in history: {}'.format(len(select_df)))
+    for index, row in select_df.iterrows():
+        if isnan(float(row['Flight #'], ), ):
+            pass
+        else:
+            logger.info('{} {}'.format(row['Location'], row['Summary'], ))
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
