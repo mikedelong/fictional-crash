0e793c60ee2e81ffb836a3e82fab89e187dfd0c6
Mike DeLong <mikedelong@outlook.com>
2020-08-28 08:36:00 -0400

    feature: install certificates to avoid complaints from the PoolManager

--- parse_ntsb.py
+++ parse_ntsb.py
@@ -6,6 +6,7 @@ from time import time
 from xml.etree import ElementTree
 from xmltodict import parse
 from pandas import DataFrame
+import certifi
 
 if __name__ == '__main__':
     time_start = time()
@@ -14,7 +15,7 @@ if __name__ == '__main__':
 
     xml_url = 'https://app.ntsb.gov/aviationquery/Download.ashx?type=xml'
 
-    http = urllib3.PoolManager()
+    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where(), )
     response = http.request('GET', xml_url, )
     data = parse(response.data)
     logger.info('got NTSB XML')
