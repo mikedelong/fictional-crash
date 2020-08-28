b45cf9a64b4c12a6cecd55dfcec673f6dfcf87b5
Mike DeLong <mikedelong@outlook.com>
2020-08-28 08:38:10 -0400

    cleanup: restrict and optimize imports

--- parse_ntsb.py
+++ parse_ntsb.py
@@ -3,9 +3,9 @@ from logging import basicConfig
 from logging import getLogger
 from time import time
 
-import urllib3
 from certifi import where
 from pandas import DataFrame
+from urllib3 import PoolManager
 from xmltodict import parse
 
 if __name__ == '__main__':
@@ -15,14 +15,14 @@ if __name__ == '__main__':
 
     xml_url = 'https://app.ntsb.gov/aviationquery/Download.ashx?type=xml'
 
-    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=where(), )
+    http = PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=where(), )
     response = http.request('GET', xml_url, )
-    data = parse(response.data)
+    data = parse(response.data, )
     logger.info('got NTSB XML')
 
     df = DataFrame(data['DATA']['ROWS']['ROW'])
     df = df.rename(axis='columns', mapper={key: key[1:] for key in data['DATA']['ROWS']['ROW'][0]}, )
-    logger.info('{}'.format(len(df)))
-    logger.info(list(df))
+    logger.info('{}'.format(len(df, ), ), )
+    logger.info(list(df, ), )
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
