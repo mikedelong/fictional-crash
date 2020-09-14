2a606649c0fdc098b582e0f73968256384972162
Mike DeLong <mikedelong@outlook.com>
2020-09-14 09:08:49 -0400

    feature: read, sort, and write fixes

---
+++ sort_fixes_and_words.py
@@ -0,0 +1,23 @@
+from json import dump
+from json import load
+from logging import INFO
+from logging import basicConfig
+from logging import getLogger
+from time import time
+
+if __name__ == '__main__':
+    time_start = time()
+    logger = getLogger(__name__, )
+    basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=INFO, )
+    logger.info('started')
+
+    with open(encoding='utf-8', file='./fixes.json', mode='r', ) as fixes_fp:
+        fixes = load(fp=fixes_fp, )
+
+    logger.info('read fixes')
+    with open(encoding='utf-8', file='./fixes.json', mode='w', ) as fixes_fp:
+        dump(obj=fixes, fp=fixes_fp, ensure_ascii=False, check_circular=False, allow_nan=False, indent=True,
+             sort_keys=True, )
+    logger.info('wrote sorted fixes')
+
+    logger.info('total time: {:5.2f}s'.format(time() - time_start))
